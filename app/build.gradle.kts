7abffa40b008a2f9982615baef02075ae17a960a
raheem <raheemideadev@gmail.com>
2026-03-22 16:45:00 +0500

    Installed dependencies;

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,20 +1,23 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.android)
+    alias(libs.plugins.compose)
 }
 
 android {
     namespace = "xyz.teamgravity.media3customvideoplayer"
+
     compileSdk {
-        version = release(36) {
+        version = release(libs.versions.sdk.compile.get().toInt()) {
             minorApiLevel = 1
         }
     }
 
     defaultConfig {
         applicationId = "xyz.teamgravity.media3customvideoplayer"
-        minSdk = 26
-        targetSdk = 36
+        minSdk = libs.versions.sdk.min.get().toInt()
+        targetSdk = libs.versions.sdk.target.get().toInt()
         versionCode = 1
         versionName = "1.0"
 
@@ -27,29 +30,48 @@ android {
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
+
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
+
     buildFeatures {
         compose = true
     }
 }
 
 dependencies {
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.compose.material3)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-    debugImplementation(libs.androidx.compose.ui.tooling)
-    debugImplementation(libs.androidx.compose.ui.test.manifest)
+    // compose
+    implementation(platform(libs.compose))
+    implementation(libs.compose.ui)
+    implementation(libs.compose.graphics)
+    implementation(libs.compose.preview)
+    implementation(libs.compose.material3)
+
+    // compose icons
+    implementation(libs.compose.icons)
+
+    // compose activity
+    implementation(libs.compose.activity)
+
+    // compose lifecycle
+    implementation(libs.compose.lifecycle)
+
+    // core
+    implementation(libs.core)
+
+    // media3
+    implementation(libs.media3)
+    implementation(libs.media3.compose)
+    implementation(libs.media3.material3)
+    implementation(libs.media3.exoplayer)
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+    compilerOptions {
+        freeCompilerArgs.addAll(
+            "-opt-in=androidx.compose.material3.ExperimentalMaterial3Api"
+        )
+    }
 }
